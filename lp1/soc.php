ed89b3b6f57d02781269fe6bf27baf9596b00951
hhoefling <penny@gmx.eu>
2022-01-29 21:00:03 +0100

    koppelmodule

---
+++ lp1/soc.php
@@ -0,0 +1,17 @@
+<?php
+
+# erzeugt soc.txt asyncron da es laenger dauerk kann
+# jedenfalls mehr als 1 bis 2 Sekunden.
+
+exec("./soc_citigo/getsoc.sh >./soc_citigo/getsoc.log 2>&1 & ", $output,$retval);
+# echo "Returned with status $retval and output:\n";
+# print_r($output);
+# echo "\n";
+
+ # Nehme letzten ermittelten Wert mit zurueck, 
+ # warte nicht auf das Ende der aktuellen abfrage
+ $soc  = file_get_contents('./soc.txt');
+ echo $soc
+
+?>
+
