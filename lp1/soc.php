8999f621573fb7fec015845944b7a5b225148b71
hhoefling <penny@gmx.eu>
2023-08-29 21:17:48 +0200

    parameter durchreichen

--- lp1/soc.php
+++ lp1/soc.php
@@ -3,13 +3,16 @@
 # erzeugt soc.txt asyncron da es laenger dauerk kann
 # jedenfalls mehr als 1 bis 2 Sekunden.
 
-exec("./soc_citigo/getsoc.sh >./soc_citigo/getsoc.log 2>&1 & ", $output,$retval);
+ $para='';
+ if( isset($_GET['para'] )) 
+    $para=$_GET['para'] ;
+  
+ exec("./soc_citigo/getsoc.sh $para >>./soc_citigo/getsoc.log 2>&1 & ", $output,$retval);
 # echo "Returned with status $retval and output:\n";
 # print_r($output);
 # echo "\n";
 
- # Nehme letzten ermittelten Wert mit zurueck, 
- # warte nicht auf das Ende der aktuellen abfrage
+ # Nehme letzten ermittelten Wert mit zurueck, warte nicht auf das Ende der aktuellen abfrage
  $soc  = file_get_contents('./soc.txt');
  echo $soc
 
