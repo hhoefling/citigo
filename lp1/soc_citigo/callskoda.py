7d40352f18e02288fa8fcd6e9b401e913f16929c
hhoefling <penny@gmx.eu>
2023-08-29 21:18:17 +0200

    optional csv erzeigen

--- lp1/soc_citigo/callskoda.py
+++ lp1/soc_citigo/callskoda.py
@@ -10,6 +10,7 @@ import paho.mqtt.client as mqtt
 from   aiohttp import ClientSession
 from   datetime import datetime
 import getopt
+import csv
     
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -32,20 +33,23 @@ lp=1
 carinfo=0
 mqttskoda=0
 mqtthost="localhost"
-
+socfile=""
+dodump=False
         
 				
 try:
-	opts, args = getopt.getopt(sys.argv[1:], 'd:p:u:l:m:CQcq', ['username=', 'password=', 'lp=', 'debug=', 'car', 'MQTT', 'mqtthost' ])
+	opts, args = getopt.getopt(sys.argv[1:], 'd:p:u:l:m:s:CQcq', ['username=', 'password=', 'socfile=', 'lp=', 'debug=', 'dump', 'car', 'MQTT', 'mqtthost=' ])
 except getopt.error as msg:
 	sys.stdout = sys.stderr
 	print(msg)
 	print("""usage: %s [-u|-p|-l|-d|-c|-q]
 	-u[sername], -p[assword]: username and passwort
+	--dump dump https requests
 	-c|--car  carinfo schreiben
 	-q|--MQTT 
 	-l[p]: Ladepunkt (1 oder 2)
-    -m --mqtthost: 
+        -m --mqtthost host
+        -s socfile 
 	-d: debug level 0,1,2""" % sys.argv[0])
 	sys.exit(2)
 for opt, arg in opts:
@@ -55,6 +59,8 @@ for opt, arg in opts:
 		password=''      # must follow username
 	elif opt in ('-p', '--password'):
 		password=arg
+	elif opt in ('-s', '--socfile'):
+		socfile=arg
 	elif opt in ('-m', '--mqtthost'):
 		mqtthost=arg
 	elif opt in ('-l', '--lp'):
@@ -65,6 +71,8 @@ for opt, arg in opts:
 		mqttskoda=1
 	elif opt in ('-d', '--debug'):
 		debug=int(arg)
+	elif opt in ('-D', '--dump'):
+		dodump=True
     
 
 if debug < 2:
@@ -75,6 +83,10 @@ _LOGGER = logging.getLogger(__name__)
 
 _LOGGER.info(f"{str(sys.argv)}")
 
+
+# Global Reader				
+##urlreader.R = urlreader.Reader(debug,dodump,'dumps')
+##urlreader.R.mkdirifneeded()
 
 
 if debug>0: print( f"callskoda.py for LP{lp} debug:{debug}") 
@@ -161,6 +173,7 @@ ToMQtt = [
     "charging_cable_connected",
     "charging_cable_locked",
     "charging_time_left",
+    "distance",
     "combustion_range",
     "outside_temperature",
     "trip_last_average_electric_consumption",
@@ -171,8 +184,14 @@ ToMQtt = [
 async def main():
     """Main method."""
 
-    all={}  # fields
+    all={}  # fields flr mqtt
+    allcsvhead=[]
+    allcsvhead.append('Date')
+    allcsvval=[]
+    allcsvval.append( datetime.now().strftime("%Y-%m-%d %H:%M:%S") )
     if debug>0: print(f'Init Skoda Connect library, version {lib_version}')
+    if debug>0: print(f'dump:{dodump}')
+    
     async with ClientSession(headers={'Connection': 'keep-alive'}) as session:
         if debug>0: print(f"Initiating new session to Skoda Connect with {username} as username")
         if debug>0:
@@ -183,7 +202,7 @@ async def main():
         if debug>0: print("Attempting to login to the Skoda Connect service")
         if debug>0: print(datetime.now())
         if await connection.doLogin():
-            if debug>0: print('Login success!')
+            if debug>=0: print('Login success!')
             if debug>0: print(datetime.now())
             if debug>0: print('Fetching vehicles associated with account.')
             await connection.get_vehicles()
@@ -215,6 +234,11 @@ async def main():
                 all['ledState']=vehicle.attrs.get('charger', {}).get('status',{}).get('ledStatusData',{}).get('ledState',{}).get('content','')
                 all['position_lat']=vehicle.position.get('lat','')
                 all['position_lng']=vehicle.position.get('lng','')
+                for x in all:
+                    val = all[x]
+                    allcsvhead.append(x)
+                    allcsvval.append( str(val).replace(".", ",") )
+                
                 
                 ds = vehicle.attrs.get('departuretimer', {}).get('timersAndProfiles', {}).get('timerList', {}).get('timer', False)
                 if( not ds == False):
@@ -262,6 +286,32 @@ async def main():
         if debug>1: print(f"{x} = {all[x]}" )
                 
                 
+    x = datetime.now()
+    fx=x.strftime("skoda_protoll_%Y%m.csv") 
+    print(f'file: ${fx} ')
+
+    try:
+        if debug>0: print(f'try to open csv')
+        f = open(fx ,'r')
+        if debug>0: print(f'ok find one')
+        # ok close 
+    except IOError:
+        print("File not accessible, create new")
+        f = open(fx ,'w')
+        writer = csv.writer(f,delimiter=';')
+        writer.writerow(allcsvhead)
+    finally:
+        f.close()
+    
+    if debug>0: print(f'open appned csv')
+    f = open(fx ,'a')
+    writer = csv.writer(f,delimiter=';')
+    writer.writerow(allcsvval)
+    f.close()
+    if debug>0: print(f'csv written')
+
+                            
+                
     # Export to MQTT for Skoda 
     mclient = mqtt.Client("openWB-skoda_broker-" + str(os.getpid()))
 
@@ -284,19 +334,38 @@ async def main():
     if debug>0: print("Write to /openwb/lp/Skoda..")
     for x in all:
         val = all[x]
+        
+        if x == 'combustion_range' :
+            field=f'openWB/set/lp/{lp}/socRange'
+            mclient.publish(str(field), payload=str(val), qos=0, retain=True)
+            if debug>0: print(f"publish socRange ({val}) to {field}")
+            #field=f'openWB/lp/{lp}/socRange'
+            #mclient.publish(str(field), payload=str(val), qos=0, retain=True)
+            #if debug>0: print(f"publish socRange ({val}) to {field}")
+            mclient.loop(timeout=2.0)
+        if x == 'distance' :
+            field=f'openWB/set/lp/{lp}/socKM'
+            mclient.publish(str(field), payload=str(val), qos=0, retain=True)
+            if debug>0: print(f"publish socKM ({val}) to {field}")
+            mclient.loop(timeout=2.0)
         if x == 'battery_level' :
+            field=f'openWB/set/lp/{lp}/manualSoc'
+            mclient.publish(str(field), payload=str(val), qos=0, retain=True)
+            if debug>0: print(f"publish soc ({val}) to {field}")
             field=f'openWB/set/lp/{lp}/%Soc'
             mclient.publish(str(field), payload=str(val), qos=0, retain=True)
-            _LOGGER.debug("MQTTT publish %s %s", field, val)
-            if debug>0: print(f"Writing ../soc.txt") 
-            file=open(f"../soc.txt","w")
+            if debug>0: print(f"publish soc ({val}) to {field}")
+            mclient.loop(timeout=2.0)
+            if len(socfile)>0:
+                if debug>0: print(f"Writing {val} to {socfile} ") 
+                file=open(socfile,"w")
             file.write( str(val) )
             file.close()             
         if x in ToMQtt :
             field=f'openWB/lp/{lp}/Skoda_{x}'
             mclient.publish(str(field), payload=str(val), qos=0, retain=True)
-            field=f'openWB/set/lp/{lp}/Skoda_{x}'
-            mclient.publish(str(field), payload=str(val), qos=0, retain=True)
+            #field=f'openWB/set/lp/{lp}/Skoda_{x}'
+            #mclient.publish(str(field), payload=str(val), qos=0, retain=True)
             _LOGGER.debug("MQTTT publish %s  %s", field, val) 
     mclient.loop(timeout=2.0)
     mclient.disconnect()
